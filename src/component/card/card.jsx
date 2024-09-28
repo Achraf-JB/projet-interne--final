123eea7e7accf7a4d3db1286d853a891afe6f983
achref <jarbouiachraf899@gmail.com>
2024-09-28 14:37:30 +0200

    add docker file and docker ignore

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Card.css'
+import './card.css'
 export const Card = () => {
   return (
     <div className='Card-Box'>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+    port: 3000, // Change the port to 3000
+    host: true,  // This exposes the server to the network
+  },
   plugins: [react()],
 })
