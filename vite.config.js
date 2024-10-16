909766829bdac2db62e73485d66a20f6fd6d1093
achref <jarbouiachraf899@gmail.com>
2024-10-16 13:59:20 +0200

    changing config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-    port: 3000, // Change the port to 3000
-    host: true,  // This exposes the server to the network
-  },
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',  // or 'localhost'
+    port: 3000,
+  },
+});
